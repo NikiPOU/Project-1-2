d80b3a7465312080e14fdf7787e36591fac1921d
ACG776 <88265663+ACG776@users.noreply.github.com>
2023-03-21 16:45:52 +0100

    Update EulerSolver.java

--- src/main/java/twentyone/EulerSolver.java
+++ src/main/java/twentyone/EulerSolver.java
@@ -8,8 +8,9 @@ public class EulerSolver {
     public Vector2d newPosition(Vector2d currentPos, Vector2d velocity){
         double x= currentPos.getX() + stepSize* velocity.getX();
         double y= currentPos.getY() + stepSize* velocity.getY();
+        double z= currentPos.getZ() + stepSize* velocity.getZ();
 
-        return new Vector2d(x,y);
+        return new Vector3d(x,y,z);
     }
 }
 
