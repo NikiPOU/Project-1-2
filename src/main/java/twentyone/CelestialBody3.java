27572aeccbd39b50d2db562f8c0e1b082d7d218f
JVEliens <115264589+JVEliens@users.noreply.github.com>
2023-03-23 12:30:24 +0100

    Add files via upload

---
+++ src/main/java/twentyone/CelestialBody3.java
@@ -0,0 +1,52 @@
+package twentyone;
+
+import java.util.Arrays;
+
+class CelestialBody3{
+    
+    private double[] initialVelocity = new double[3];
+    private double[] initialPosition = new double[3];
+    private double mass;
+    private double[] currentForce = new double[3];
+
+    public CelestialBody3(double[] initialVelocity, double[] initialPosition, double mass){
+        this.initialVelocity = initialVelocity;
+        this.initialPosition = initialPosition;
+        this.mass = mass;
+    }
+
+    public void setNewVelocity(double[] newVelocity){
+        this.initialVelocity = newVelocity;
+    }
+
+    public void setNewPostion(double[] newPosition) {
+        this.initialPosition = newPosition;
+    }
+
+    public void setNewForce(double[] newForce){
+        this.currentForce = newForce;
+    }
+
+    public double[] getVelocity(){
+        return initialVelocity;
+    }
+
+    public double[] getPosition(){
+        return initialPosition;
+    }
+
+    public double getMass(){
+        return mass;
+    }
+
+    public double[] getForce(){
+        return currentForce;
+    }
+
+    public String toString() {
+        String pos = Arrays.toString(initialPosition);
+        String vel = Arrays.toString(initialVelocity);
+        return "[Celestial body: position: " + pos + ", velocity: " + vel + ", mass: " + mass;
+    }
+    
+}
