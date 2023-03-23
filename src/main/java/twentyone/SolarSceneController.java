703afd08445434490ba51c8106dc2f283c8c72aa
JVEliens <115264589+JVEliens@users.noreply.github.com>
2023-03-23 12:21:21 +0100

    Add files via upload

--- src/main/java/twentyone/SolarSceneController.java
+++ src/main/java/twentyone/SolarSceneController.java
@@ -33,6 +33,19 @@ public class SolarSceneController implements Initializable {
     int sunx = 845;
     int suny = 445;
 
+    CelestialBody3 sunCB;
+    CelestialBody3 mercuryCB;
+    CelestialBody3 venusCB;
+    CelestialBody3 earthCB;
+    CelestialBody3 moonCB;
+    CelestialBody3 marsCB;
+    CelestialBody3 jupiterCB;
+    CelestialBody3 saturnCB;
+    CelestialBody3 titanCB;
+    CelestialBody3 neptuneCB;
+    CelestialBody3 uranusCB;
+    CelestialBody3[] bodies = new CelestialBody3[11];
+
     @FXML
     Label probeCoords;
     @FXML
@@ -66,10 +79,11 @@ public class SolarSceneController implements Initializable {
     @FXML
     Group dots;
 
-    int i = 180;
+    int hours = -1;
     int days = -1;
     int months = -1;
     int years = 0;
+    int divider = 2100000;
     double ex;
     double ey;
     double spax;
@@ -83,12 +97,18 @@ public class SolarSceneController implements Initializable {
 
     private Timeline timeline;
 
+    int k=0;
+
     //1 = 2000000
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         dots = new Group();
         root.getChildren().add(dots);
+        Physics_Engine physics = new Physics_Engine();
+        Unreal_Engine unreal = new Unreal_Engine();
         probeCoords.setText("Currect probe coords: " + probeCoords2);
         distanceTitan.setText("Distance to Titan: " + distanceTitan2);
         launchCoords.setText("Launch coords: " + launchCoords2);
@@ -102,22 +122,69 @@ public class SolarSceneController implements Initializable {
         //one can add a specific action when the keyframe is reached
         EventHandler<ActionEvent> movement = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t) {
-                ex = 120*Math.cos(Math.toRadians(i))+sunx;
-                ey = 120*Math.sin(Math.toRadians(i))+suny;
-                double mx = 15*Math.cos(Math.toRadians(12*i))+ex+7;
-                double my = 15*Math.sin(Math.toRadians(12*i))+ey+7;
-                double mex = 59*Math.cos(Math.toRadians(4*i))+sunx;
-                double mey = 59*Math.sin(Math.toRadians(4*i))+suny;
-                double vx = 80*Math.cos(Math.toRadians(1.7*i))+sunx;
-                double vy = 80*Math.sin(Math.toRadians(1.7*i))+suny;
-                double max = 180*Math.cos(Math.toRadians(0.52*i))+sunx;
-                double may = 180*Math.sin(Math.toRadians(0.52*i))+suny;
-                double jx = 419*Math.cos(Math.toRadians(0.08*i))+sunx;
-                double jy = 419*Math.sin(Math.toRadians(0.08*i))+suny;
-                sx = 700*Math.cos(Math.toRadians(0.034*i))+sunx;
-                sy = 700*Math.sin(Math.toRadians(0.034*i))+suny;
-                tx = 40*Math.cos(Math.toRadians(22.64*i))+sx+40;
-                ty = 40*Math.sin(Math.toRadians(22.64*i))+sy+20;
+                for (int i = 0; i < 100; i++) {
+                    for (int j = 0; j < bodies.length; j++) {
+                        bodies = unreal.Eulers(bodies, j, 3600);
+                    }
+                    hours++;
+                    k++;
+                    if(hours % 24 == 0){
+                        days++;
+                        hours = 0;
+                        if(days % 30 == 0){
+                            months++;
+                            days = 0;
+                            if(months/12 == 1){
+                                months = 0;
+                                years++;
+                            }
+                        }
+                    }
+                }
+                TimeElapsed.setText("Time Elapsed: " + years + " years, " + months + " months, " + days + " days and " + hours + "hours");
+                
+                // physics.sumUpdate(bodies, 3);
+                // physics.euler_solver(bodies, 3, 1);
+                double[] meps = bodies[1].getPosition();
+                double[] veps = bodies[2].getPosition();
+                double[] eaps = bodies[3].getPosition();
+                double[] mops = bodies[4].getPosition();
+                double[] maps = bodies[5].getPosition();
+                double[] jups = bodies[6].getPosition();
+                double[] saps = bodies[7].getPosition();
+                double[] tips = bodies[8].getPosition();
+                // ex = 120*Math.cos(Math.toRadians(i))+sunx;
+                // ey = 120*Math.sin(Math.toRadians(i))+suny;
+                ex = sunx + eaps[0]/divider;
+                ey = suny + eaps[1]/divider;
+                // double mx = 15*Math.cos(Math.toRadians(12*i))+ex+7;
+                // double my = 15*Math.sin(Math.toRadians(12*i))+ey+7;
+                // double mex = 59*Math.cos(Math.toRadians(4*i))+sunx;
+                // double mey = 59*Math.sin(Math.toRadians(4*i))+suny;
+                // double vx = 80*Math.cos(Math.toRadians(1.7*i))+sunx;
+                // double vy = 80*Math.sin(Math.toRadians(1.7*i))+suny;
+                // double max = 180*Math.cos(Math.toRadians(0.52*i))+sunx;
+                // double may = 180*Math.sin(Math.toRadians(0.52*i))+suny;
+                // double jx = 419*Math.cos(Math.toRadians(0.08*i))+sunx;
+                // double jy = 419*Math.sin(Math.toRadians(0.08*i))+suny;
+                // sx = 700*Math.cos(Math.toRadians(0.034*i))+sunx;
+                // sy = 700*Math.sin(Math.toRadians(0.034*i))+suny;
+                // tx = 40*Math.cos(Math.toRadians(22.64*i))+sx+40;
+                // ty = 40*Math.sin(Math.toRadians(22.64*i))+sy+20;
+                double mx = sunx + 8 + 15*Math.cos(k*0.008) + mops[0]/divider; //8+ 15*Math.cos(i) + 
+                double my = suny + 8 + 15*Math.sin(k*0.008) + mops[1]/divider;//8+ 15*Math.sin(i) +
+                double mex = sunx + meps[0]/divider;
+                double mey = suny + meps[1]/divider;
+                double vx = sunx + veps[0]/divider;
+                double vy = suny + veps[1]/divider;
+                double max = sunx + maps[0]/divider;
+                double may = suny + maps[1]/divider;
+                double jx = sunx + jups[0]/divider;
+                double jy = suny + jups[1]/divider;
+                sx = sunx + saps[0]/divider;
+                sy = suny + saps[1]/divider;
+                tx = sunx + tips[0]/divider;
+                ty = suny + tips[1]/divider;
                 earth.setLayoutX(ex);
                 earth.setLayoutY(ey);
                 moon.setLayoutX(mx);
@@ -134,20 +201,10 @@ public class SolarSceneController implements Initializable {
                 saturn.setLayoutY(sy);
                 titan.setLayoutX(tx);
                 titan.setLayoutY(ty);
-                days++;
-                if(days % 30 == 0){
-                    months++;
-                    days = 0;
-                    if(months/12 == 1){
-                        months = 0;
-                        years++;
-                    }
-                }
-                TimeElapsed.setText("Time Elapsed: " + years + " years, " + months + " months and " + days + " days");
-                i++;
             }
         };
         EventHandler<ActionEvent> spaceMovement = new EventHandler<ActionEvent>() {
+
             @Override
             public void handle(ActionEvent event) {
                 double satdis = Math.sqrt(((sx-spax)*(sx-spax))+((sy-spay)*(sy-spay)));
@@ -186,6 +243,7 @@ public class SolarSceneController implements Initializable {
                     circle.setOpacity(0.4);
                     dotList.add(circle);
                     dots.getChildren().add(circle);
+                    
                 } else {
                     spax = ex+2;
                     spay = ey+2;
@@ -197,7 +255,10 @@ public class SolarSceneController implements Initializable {
             }
             
         };
-         
+
+        
+
+        initiateCB();
         KeyFrame keyFrame = new KeyFrame(Duration.millis(100), movement);
         KeyFrame keyFrame2 = new KeyFrame(Duration.millis(100), spaceMovement);
          
@@ -221,5 +282,67 @@ public class SolarSceneController implements Initializable {
     @FXML
     private void switchToStart() throws IOException {
         App.setRoot("StartScene");
+    }
+
+    private void initiateCB(){
+        double[] merpos = {(long) 7833268.43923962, (long) 44885949.3703908, (long) 2867693.20054382};
+        double[] mervel = {-57.4967480139828, 11.52095127176, 6.21695374334136};
+
+        double[] venusvel = {-34.0236737066136, -8.96521274688838, 1.84061735279188};
+        double[] venuspos = {(long) -28216773.9426889, (long) 103994008.541512, (long) 3012326.64296788};
+
+        double[] sunvel = {0, 0, 0};
+        double[] sunpos = {0, 0, 0};
+        
+
+        double[] earthvel = {5.05251577575409, -29.3926687625899,  0.00170974277401292};
+        double[] earthpos = {(long) -148186906.893642, (long) -27823158.5715694, (long) 33746.8987977113};
+
+
+        double[] moonvel = {4.34032634654904, -30.0480834180741, -0.0116103535014229};
+        double[] moonpos = {(long) -148458048.395164, (long) -27524868.1841142, (long) 70233.6499287411};
+
+        double[] marsvel = {-17.6954469224752, -13.4635253412947, 0.152331928200531};
+        double[] marspos = {(long) -159116303.422552, (long) 189235671.561057, (long) 7870476.08522969};
+
+
+        double[] jupivel = {-4.71443059866156, 12.8555096964427, 0.0522118126939208};
+        double[] jupipos = {(long) 692722875.928222, (long) 258560760.813524, (long) -16570817.7105996};
+
+        double[] satuvel = {4.46781341335014, 8.23989540475628,  -0.320745376969732};
+        double[] satupos = {(long) 1253801723.95465, (long) -760453007.810989, (long) -36697431.1565206};
+
+        double[] titvel = {8.99593229549645, 11.1085713608453, -2.25130986174761};
+        double[] titpos = {(long) 1254501624.95946, (long) -761340299.067828, (long) -36309613.8378104};
+
+        double[] nepvel = {0.447991656952326, 5.44610697514907, -0.122638125365954};
+        double[] neppos = {(long) 4454487339.09447, (long) -397895128.763904, (long) -94464151.3421107};
+
+        double[] uravel = {-5.12766216337626, 4.22055347264457, 0.0821190336403063};
+        double[] urapos = {(long) 1958732435.99338, (long) 2191808553.21893, (long) -17235283.8321992};
+
+        sunCB = new CelestialBody3(sunvel, sunpos, 1.991E30);
+        earthCB = new CelestialBody3(earthvel, earthpos, 5.97219E+24);
+        venusCB = new CelestialBody3(venusvel, venuspos, 4.8685E+24);
+        mercuryCB = new CelestialBody3(mervel, merpos, 3.302E+23);
+        moonCB = new CelestialBody3(moonvel, moonpos, 7.3491E22);
+        marsCB = new CelestialBody3(marsvel, marspos, 6.4171E+23);
+        jupiterCB = new CelestialBody3(jupivel, jupipos, 1.89819E+27);
+        saturnCB = new CelestialBody3(satuvel, satupos, 5.6834E+26);
+        titanCB = new CelestialBody3(titvel, titpos, 1.34553E+23);
+        neptuneCB = new CelestialBody3(nepvel, neppos, 1.02409E+26);
+        uranusCB = new CelestialBody3(uravel, urapos, 86.813E+24);
+
+        bodies[0] = sunCB;
+        bodies[3] = earthCB;
+        bodies[2] = venusCB;
+        bodies[5] = marsCB;
+        bodies[9] = uranusCB;
+        bodies[10] = neptuneCB;
+        bodies[1] = mercuryCB;
+        bodies[4] = moonCB;
+        bodies[8] = titanCB;
+        bodies[7] = saturnCB;
+        bodies[6] = jupiterCB;
     }
 }
