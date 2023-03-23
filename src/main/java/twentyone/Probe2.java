72904ffeed4e5ad73d76f3b623824e6a73508556
Mika Wauben <m.l.wauben@student.tudelft.nl>
2023-03-23 13:59:47 +0100

    Add Jaime's probe

---
+++ src/main/java/twentyone/Probe2.java
@@ -0,0 +1,9 @@
+package twentyone;
+class Probe2 extends CelestialBody3{
+    
+    public Probe2(double[] velocity, double[] position, double mass){
+        super(velocity, position, mass);
+    }
+
+    //this is just to make life ez. we will add the engine stuff later
+}
