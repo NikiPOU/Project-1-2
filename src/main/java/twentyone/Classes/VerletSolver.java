7b238ba87e12d2a1fe05abaa757d6ff04e10974f
ACG776 <88265663+ACG776@users.noreply.github.com>
2023-05-02 00:08:21 +0200

    VerletSolver added with issues

---
+++ src/main/java/twentyone/Classes/VerletSolver.java
@@ -0,0 +1,51 @@
+package twentyone.Classes;
+
+public class VerletSolver extends Solver
+{
+
+
+    public CelestialBody[] verlet(CelestialBody[] allBodies, int bodyIndex, double stepsize) // to be changed with the celestial body class (CelestialBody[] ..)
+    {
+        
+        //take values from the rest of the program (CelestialBody)
+
+    
+        //position verlet
+    
+        //instance variables
+        Vector3d prevPos = allBodies[bodyIndex].getPosition();
+        Vector3d prevVel = allBodies[bodyIndex].getVelocity();
+
+        Vector3d prevA = sumOf_Forces(allBodies, bodyIndex) / allBodies[desiredPlanet.mass]; //computing the acceleration
+
+    
+        //double time = 0.0;
+        
+        double dt = stepsize; //time step determining time passed between each iteration
+
+        
+        Vector3d newPos = prevPos.add(prevVel.mul(dt)).add(prevA.mul(0.5 * dt * dt)); //equation for finding the new x according to verlet PosX + v * dt + 0.5 * a * dt * dt;
+
+        allBodies[bodyIndex].setNewPostion(newPos); //adding the new position to the array allBodies
+        
+        //compute the new acceleration
+        double newA = sumOf_Forces(allBodies, bodyIndex) / allBodies[desiredPlanet.mass]; //computing the acceleration (currently incorrect implementation, to be updated)
+        
+        Vector3d newVel =  prevVel.add(newA.add(prevA).mul(0.5 * dt));//equation to find the next velocity according to verlet prevVel + 0.5 * (newa + preva) * dt;
+
+        allBodies.setNewVelocity(newVel); //add the new velocity to the Celestial Bodies array
+
+
+        return allBodies;
+        
+
+        // return allBodies
+
+        
+    }
+
+    
+
+
+
+}
