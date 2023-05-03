f45c7be570de5787cd34496b8c98a0f260292edc
JVEliens <j.eliens@student.maastrichtuniversity.nl>
2023-05-03 13:02:46 +0200

    StartSceneController changed

--- src/main/java/twentyone/Controllers/StartSceneController.java
+++ src/main/java/twentyone/Controllers/StartSceneController.java
@@ -8,6 +8,6 @@ public class StartSceneController {
 
     @FXML
     private void switchToSolar() throws IOException {
-        App.setRoot("fxml/SolarScene");
+        App.setRoot("fxml/SolarScene3D");
     }
 }
